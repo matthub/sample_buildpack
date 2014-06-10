4619566cc076d7402f3550fdcde3281b26b64303
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-06-10 10:46:07 +0530

    Update language_pack.rb

--- lib/language_pack.rb
+++ lib/language_pack.rb
@@ -1,1 +1,16 @@
-bg
+require "language_pack/java"
+require "language_pack/geronimo"
+
+# General Language Pack module
+module LanguagePack
+
+  # detects which language pack to use
+  # @param [Array] first argument is a String of the build directory
+  # @return [LanguagePack] the {LanguagePack} detected
+  def self.detect(*args)
+    Dir.chdir(args.first)
+
+    
+  end
+
+end
