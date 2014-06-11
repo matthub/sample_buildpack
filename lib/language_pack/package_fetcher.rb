456949e2c076858629b9948596874b6ff8b73b38
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-06-11 14:28:22 +0530

    Update package_fetcher.rb

--- lib/language_pack/package_fetcher.rb
+++ lib/language_pack/package_fetcher.rb
@@ -3,7 +3,7 @@ require "uri"
 require "base64"
 
 module LanguagePack
-  module PackageFetcher
+  class PackageFetcher
 
     VENDOR_URL = "https://s3.amazonaws.com/heroku-jvm-langpack-java"
     PACKAGES_CONFIG = File.join(File.dirname(__FILE__), "../../config/packages.yml")
