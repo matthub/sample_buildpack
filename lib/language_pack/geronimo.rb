b044ae19b048c267e3bad10c141d5ef5e8abbc07
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-06-25 16:12:58 +0530

    Update geronimo.rb

--- lib/language_pack/geronimo.rb
+++ lib/language_pack/geronimo.rb
@@ -74,9 +74,8 @@ module LanguagePack
     end
     def move_geronimo_to_root
       run_with_err_output("mv #{geronimo_home}/* . && rm -rf #{geronimo_home}")
-      Dir.chdir("#{@build_path}/bin") do
-          run_with_err_output("deploy --user system --password manager deploy --inPlace #{@build_path/app_home}")
-      end
+      "console" => "deploy --user system --password manager deploy --inPlace #{@build_path/app_home}"
+         
     end
     def geronimo_home
       ".geronimo_home"
