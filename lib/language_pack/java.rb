d6875578289c3b44ac7974fe397175cfbe594815
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-06-25 10:58:44 +0530

    Update java.rb

--- lib/language_pack/java.rb
+++ lib/language_pack/java.rb
@@ -42,8 +42,9 @@ module LanguagePack
       download_jdk jdk_tarball
 
       puts "Unpacking JDK to #{jdk_dir}"
+       download_start_time = Time.now
       tar_output = run_with_err_output "tar pxzf #{jdk_tarball} -C #{jdk_dir}"
-
+       puts "(#{(Time.now - download_start_time).duration})"
       FileUtils.rm_rf jdk_tarball
       unless File.exists?("#{jdk_dir}/bin/java")
         puts "Unable to retrieve the JDK"
