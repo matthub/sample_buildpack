4ee580a3eb78820382c77a8b47dead7625d20d66
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-07-01 15:02:50 +0530

    Update geronimo.rb

--- lib/language_pack/geronimo.rb
+++ lib/language_pack/geronimo.rb
@@ -27,28 +27,9 @@ module LanguagePack
     end
     def compile
       Dir.chdir(@build_path) do
-       
-       #FileUtils.mkdir_p app_home
-      #  run_with_err_output("mv * #{app_home}")
-       
-        
-      
         install_geronimo
-      
         copy_webapp_to_geronimo
-        
-      
         move_geronimo_to_root
-        
-        Dir.chdir("deploy") do
-          puts "***files in deploy folder\n"
-          files=Dir.glob("*")      
-           puts files
-      
-         end
-         
-      
-         
       end
     end
    
@@ -58,45 +39,31 @@ module LanguagePack
        FileUtils.mkdir_p geronimo_home
        geronimo_package = geronimo_config["repository_root"]
        filename = geronimo_config["filename"]
-        #FileUtils.mkdir_p geronimo_dir
-       #geronimo_zip="#{geronimo_dir}/geronimo.zip"
        puts "------->Downloading #{filename}  from #{geronimo_package}"
        download_start_time = Time.now
        system("curl #{geronimo_package}/#{filename} -s -o #{filename}")
-      
-       #FileUtils.mv filename, geronimo_zip
        puts "(#{(Time.now - download_start_time).duration})"
        puts "------->Unpacking Geronimo"
        download_start_time = Time.now
        system "unzip -oq -d #{geronimo_home} #{filename} 2>&1"
        run_with_err_output("mv #{geronimo_home}/geronimo-tomcat*/* #{geronimo_home} && " + "rm -rf #{geronimo_home}/geronimo-tomcat*")
-        run_with_err_output("rm -rf geronimo.zip")
+       run_with_err_output("rm -rf geronimo.zip")
        puts "(#{(Time.now - download_start_time).duration})"
         
-      unless File.exists?("#{geronimo_home}/bin/geronimo.sh")
-        puts "Unable to retrieve Geronimo"
-        exit 1
-      end
+       unless File.exists?("#{geronimo_home}/bin/geronimo.sh")
+         puts "Unable to retrieve Geronimo"
+         exit 1
+       end
       
-       
     end
-  
-    
-     def geronimo_config
+    def geronimo_config
       YAML.load_file(File.expand_path(GERONIMO_CONFIG))
     end
     def copy_webapp_to_geronimo
         run_with_err_output("mkdir -p #{geronimo_home}/deploy && mv * #{geronimo_home}/deploy")
-      
-       #run_with_err_output("jar -cvf MyWebApp.war #{geronimo_home}/deploy")
-       
-       
-      
     end
     def move_geronimo_to_root
       run_with_err_output("mv #{geronimo_home}/* . && rm -rf #{geronimo_home}")
-      
-         
     end
     def geronimo_home
       ".geronimo_home"
@@ -104,14 +71,8 @@ module LanguagePack
     def app_home
       ".app_home"
     end
-    
-   
-    
-    
-     def run_with_err_output(command)
+    def run_with_err_output(command)
       %x{ #{command} 2>&1 }
     end
-    
-    
   end
 end
