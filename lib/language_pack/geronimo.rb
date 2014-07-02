6e537bea3662a2e22eee8232dd5059879d096673
mansifulsundar <mansifulsundar@users.noreply.github.com>
2014-07-02 12:31:11 +0530

    Update geronimo.rb

--- lib/language_pack/geronimo.rb
+++ lib/language_pack/geronimo.rb
@@ -30,7 +30,7 @@ module LanguagePack
         install_geronimo
         copy_webapp_to_geronimo
         move_geronimo_to_root
-        
+        $./var/config/config-substitutions.properties[HTTPPort]=$PORT
       end
     end
    
